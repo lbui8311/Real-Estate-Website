eece1005170f0b9e5a06a671e3af69581f6adea3
Luan Bui <119889713+lbui8311@users.noreply.github.com>
2024-05-01 10:03:48 +0000

    Added Frontend Developments

--- django_project/views.py
+++ django_project/views.py
@@ -1,19 +1,20 @@
-#from django.http import HttpResponse
-#import requests
 from django.shortcuts import render
-#from ipware import get_client_ip
 from pandas import pandas as pd
 
 
 def index(request):
   zillow = pd.read_csv('zillow_states.csv')
-  #print(df)
-  #df2024 = zillow['RegionName']
 
-  California = zillow.iloc[0, 5::12]
+  # California = zillow.iloc[0, 5::12]
   date_group = []
   state_group = []
   price_group = []
+
+  numStates = 0
+
+  for state in zillow['RegionName']:
+    state_group.append(state)
+    numStates = numStates + 1
 
   for col in zillow.columns:
     if col == '1/31/2000':
@@ -27,21 +28,25 @@ def index(request):
     if col == '1/31/2024':
       date_group.append(col)
 
-  for state in zillow['RegionName']:
-    state_group.append(state)
+  for numb in range(numStates):
+    for col in zillow.columns:
+      if col == '1/31/2000':
+        price_group.append(zillow[col][numb])
+      if col == '1/31/2006':
+        price_group.append(zillow[col][numb])
+      if col == '1/31/2012':
+        price_group.append(zillow[col][numb])
+      if col == '1/31/2018':
+        price_group.append(zillow[col][numb])
+      if col == '1/31/2024':
+        price_group.append(zillow[col][numb])
 
-  test = zillow
+  # print(price_group)
 
-  print(date_group)
-  data = {
-      "California_1": California[0],
-      "California_2": California[3],
-      "California_3": California[6],
-      "California_4": California[9],
-      "California_5": California[12],
-      "California_6": California[15],
-      "States": state_group,
-      "Dates": date_group
-  }
+  data = {"States": state_group, "Dates": date_group, "Prices": price_group}
 
   return render(request, 'index.html', context=data)
+
+def about(request):
+  
+  return render(request, 'about.html')
